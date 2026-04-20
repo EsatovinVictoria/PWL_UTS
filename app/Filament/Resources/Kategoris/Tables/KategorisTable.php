d767a73546492a19eed24eaa0416d0f01ac06179
Esatovin Ebenhaezer Victoria <esatovinv@gmail.com>
2026-04-20 18:45:59 +0700

    Mengedit tampilan tabel untuk kategori

--- app/Filament/Resources/Kategoris/Tables/KategorisTable.php
+++ app/Filament/Resources/Kategoris/Tables/KategorisTable.php
@@ -8,6 +8,7 @@ use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Filament\Actions\DeleteAction;
 
 class KategorisTable
 {
@@ -15,10 +16,18 @@ class KategorisTable
     {
         return $table
             ->columns([
+                TextColumn::make('kategori_id')
+                    ->label('ID')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('kategori_kode')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->label('Kode Kategori'),
                 TextColumn::make('kategori_nama')
-                    ->searchable(),
+                    ->searchable()
+                    ->sortable()
+                    ->label('Nama Kategori'),
             ])
             ->filters([
                 //
@@ -26,6 +35,7 @@ class KategorisTable
             ->recordActions([
                 ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
