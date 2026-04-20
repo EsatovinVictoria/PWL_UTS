cc212fd82e843c9d21d41e49e8ab1314dc4837ef
Esatovin Ebenhaezer Victoria <esatovinv@gmail.com>
2026-04-20 13:39:55 +0700

    Mengedit tampilan form pembuatan kategori

--- app/Filament/Resources/Kategoris/Schemas/KategoriForm.php
+++ app/Filament/Resources/Kategoris/Schemas/KategoriForm.php
@@ -4,6 +4,8 @@ namespace App\Filament\Resources\Kategoris\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Section;
+
 
 class KategoriForm
 {
@@ -11,10 +13,28 @@ class KategoriForm
     {
         return $schema
             ->components([
-                TextInput::make('kategori_kode')
-                    ->required(),
-                TextInput::make('kategori_nama')
-                    ->required(),
-            ]);
+                Section::make('Informasi Kategori')
+                ->icon('heroicon-o-information-circle')
+                ->description('Masukkan informasi kategori dengan benar.')
+                ->schema([
+                    TextInput::make('kategori_kode')
+                        ->label('Kode Kategori')
+                        ->prefixIcon('heroicon-o-hashtag')
+                        ->required()
+                        ->maxLength(10)
+                        ->unique(ignoreRecord: true)
+                        ->helperText('Kode kategori harus unik.')
+                        ->placeholder('Masukkan kode kategori')
+                        ->validationMessages([
+                            'unique' => 'Kode kategori sudah digunakan. Harap gunakan kode lain.',
+                        ]),
+                    TextInput::make('kategori_nama')
+                        ->label('Nama Kategori')
+                        ->prefixIcon('heroicon-o-tag')
+                        ->required()
+                        ->maxLength(100)
+                        ->placeholder('Masukkan nama kategori'),    
+                ])
+            ])->columns(1);
     }
 }
