0950251f763a3c32cb7b56f75345a58b67512b65
Esatovin Ebenhaezer Victoria <esatovinv@gmail.com>
2026-04-20 13:22:16 +0700

    Menambahkan resource untuk menu kategori

---
+++ app/Filament/Resources/Kategoris/Schemas/KategoriInfolist.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Resources\Kategoris\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class KategoriInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('kategori_kode'),
+                TextEntry::make('kategori_nama'),
+            ]);
+    }
+}
